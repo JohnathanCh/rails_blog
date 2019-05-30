8ac7f5b998fae6536d3f720aa5f318a5b0c45778
github username <github email address>
2019-05-30 14:09:37 -0400

    basic login working

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,13 @@ Rails.application.routes.draw do
   post "/posts", to: "posts#create"
   get "/posts/:id", to: "posts#show", as: 'post'
 
-  get "/signup", to: "sessions#new", as: "singup"
+  get "/signup", to: "sessions#new", as: "signup"
 
   post "/users", to: "users#create"
   get "/users/:id", to: "users#show", as: "user"
 
   get "/login", to: "sessions#new", as: "login"
   post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy", as: "logout"
 
 end
